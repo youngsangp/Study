2666a002205c8b3ba69c16f3d0828b99e40c424d
youngsangp <ini6351@gmail.com>
2021-07-18 15:32:54 +0900

    hash2

--- src/test/java/TEST.java
+++ src/test/java/TEST.java
@@ -13,4 +13,16 @@ public class TEST {
         String result = hash1.hash1(args1, args2);
         assertEquals("leo", result);
     }
+
+    @Test
+    void hash2(){
+        String[] args1 = {"119", "97674223", "1195524421"};
+        String[] args2 = {"123","456","789"};
+        String[] args3 = {"12","123","1235","567","88"};
+
+        hash2 hash2 = new hash2();
+
+        boolean reuslt = hash2.hash2(args3);
+        assertEquals(false, reuslt);
+    }
 }
