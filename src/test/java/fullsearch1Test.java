2bb90a550740f50fa2fc9a3f48f377d540dd759a
youngsangp <ini6351@gmail.com>
2021-08-01 19:08:49 +0900

    fullsearch1 start/finish

--- src/test/java/fullsearch1Test.java
+++ src/test/java/fullsearch1Test.java
@@ -1,16 +1,19 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class fullsearch1Test {
     @Test
     void fullsearch1Test() {
-        String args1 = "17";
+        //int[] args1 = {1,2,3,4,5};
+        int[] args1 = {1,3,2,4,2};
 
         fullsearch1 fullsearch1 = new fullsearch1();
 
-        int result = fullsearch1.fullsearch1(args1);
-        int expected = 3;
-        assertEquals(expected, result);
+        int[] result = fullsearch1.fullsearch1(args1);
+        //int[] expected = {1};
+        int[] expected = {1,2,3};
+        assertArrayEquals(expected, result);
     }
 }
