79978627a4d81d047bcb3d68e76bf08169ba2d1d
youngsangp <ini6351@gmail.com>
2021-08-05 23:30:21 +0900

    fullsearch3 start/finish

---
+++ src/test/java/fullsearch3Test.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class fullsearch3Test {
+    @Test
+    void fullsearch3Test() {
+
+        int args1 = 10;
+        int args2 = 2;
+
+//        int args1 = 10;
+//        int args2 = 2;
+
+        fullsearch3 fullsearch3 = new fullsearch3();
+
+        int[] result = fullsearch3.fullsearch3(args1, args2);
+
+        int[] expected = {4, 3};
+//        int[] expected = {3, 3};
+        assertArrayEquals(expected, result);
+    }
+}
