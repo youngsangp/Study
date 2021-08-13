20201d2b3c20801d721a22a89a22942514a9d258
youngsangp <ini6351@gmail.com>
2021-08-13 23:23:37 +0900

    2021 KAKAO BLIND RECRUITMENT 2 START/FINISH

---
+++ src/test/java/kakaoblind2021/menurenewerTest.java
@@ -0,0 +1,25 @@
+package kakaoblind2021;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class menurenewerTest {
+    @Test
+    void name() {
+        //String[] args1 = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
+        String[] args1 = {"XYZ", "XWY", "WXA"};
+        //int[] args2 = {2,3,4};
+        int[] args2 = {2,3,4};
+
+        menurenewer menurenewer = new menurenewer();
+        String[] result = menurenewer.solution(args1, args2);
+
+        //String[] expected = {"AC", "ACDE", "BCFG", "CDE"};
+        String[] expected = {"WX", "XY"};
+
+//        assertEquals(expected, expected);
+        assertArrayEquals(expected, expected);
+    }
+}
